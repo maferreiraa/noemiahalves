f0731e761b7f740d36d0a8f019a229ca5cce5cf7
maferreiraa <maferreiraa@hotmail.com>
2026-05-14 16:46:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Troque estes links pelos links oficiais dos ingressos no Sympla.
-const SYMPLA_GRATUITO = 'https://www.sympla.com.br/';
-const SYMPLA_VIP = 'https://www.sympla.com.br/';
-
-// Exemplo: se você quiser diferenciar os botões depois, basta colocar classes específicas.
-document.querySelectorAll('a[href="https://www.sympla.com.br/"]').forEach((link) => {
-  link.addEventListener('click', () => {
-    console.log('Clique em CTA Sympla');
-  });
-});
+document.querySelectorAll('a[href^="#"]').forEach(link=>{link.addEventListener('click',e=>{const target=document.querySelector(link.getAttribute('href'));if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'});}})});
